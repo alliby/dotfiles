ee29d5ab394c6ba4f98b486406e56342f7e7e4f5
alliby <mouhamir2@gmail.com>
2024-03-26 06:47:38 +0100

    remove dpi scale env variables

--- host/environment.nix
+++ host/environment.nix
@@ -1,20 +1,8 @@
 { ... }: {
   environment.variables = {
-    # Scaling
-    # GDK_SCALE = "1.4";
-    # GDK_DPI_SCALE = "1";
-    # QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-
     # Set Helix as Default Editor
     EDITOR = "hx"; 
     TERMINAL = "alacritty";
     _JAVA_AWT_WM_NONREPARENTING = "1";
-  };
-
-  # session variables
-  environment.sessionVariables = {
-    # XDG Variables
-    XDG_CACHE_HOME  = "\${HOME}/.cache";
-    XDG_CONFIG_HOME = "\${HOME}/.config";
   };
 }
