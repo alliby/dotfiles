cf4f18a34acd286527879ccbfd3e538e20fe69d9
alliby <mouhamir2@gmail.com>
2024-03-26 06:48:50 +0100

    Update xorg config

--- host/system.nix
+++ host/system.nix
@@ -43,34 +43,48 @@
   # Enable the X11 windowing system.
   services.xserver = {
     enable = true;
-    excludePackages = [ pkgs.xterm pkgs.nano ];
+    excludePackages = [ pkgs.xterm ];
 
-    # Tiling Windows Manager
+    # # Tiling Windows Manager
     windowManager.bspwm.enable = true;
     windowManager.i3.enable = true;
-    windowManager.awesome.enable = true;
 
-    # Configure keymap in X11
-    layout = "us,ara";
-    # See Full Keyboard Layout Keybinds "grep "grp:.*toggle" /usr/share/X11/xkb/rules/base.lst"
-    xkbOptions = "grp:alt_shift_toggle";
+    # # Configure keymap in X11
+    # # See Full Keyboard Layout Keybinds "grep "grp:.*toggle" /usr/share/X11/xkb/rules/base.lst"
+    xkb = {
+      layout = "us,ara";
+      options = "grp:alt_shift_toggle";
+    };
 
-    # Enable touchpad support (enabled default in most desktopManager).
+    # # Enable touchpad support (enabled default in most desktopManager).
     libinput.enable = true;
     libinput.touchpad.naturalScrolling = true;
   };
 
   # Enable sound.
   sound.enable = true;
-  hardware.pulseaudio = {
+  security.rtkit.enable = true;
+  services.pipewire = {
     enable = true;
-    support32Bit = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    wireplumber.configPackages = [
+      (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
+        bluez_monitor.properties = {
+        	["bluez5.enable-sbc-xq"] = true,
+        	["bluez5.enable-msbc"] = true,
+        	["bluez5.enable-hw-volume"] = true,
+        	["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+        }
+      '')
+    ];
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.cargo = {
     isNormalUser = true;
-    extraGroups = ["wheel" "networkmanager" "audio" "video" "render" "bluetooth" "dialout"];
+    extraGroups = ["wheel" "networkmanager" "audio" "video" "render" "bluetooth" "dialout" "input" "aspizu" ];
   };
 
   # Disable sudo
